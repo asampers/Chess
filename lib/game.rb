7f07c054d90b38694aa6cd63e2feef9c02c3c795
nanarama <annasampers@gmail.com>
2022-08-05 14:36:09 -0500

    Adjust board variable to create new Board object

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,7 @@
 require_relative '../lib/player.rb'
 require_relative '../lib/pieces.rb'
 require_relative '../lib/moves.rb'
+require_relative '../lib/board.rb'
 
 module Display
 
@@ -18,32 +19,7 @@ module Display
     selection
   end
 
-  def place_initial_pieces()
-    pieces.each do |teams|
-      teams.each do |piece|
-      x = piece.starting[0]
-      y = piece.starting[1]
-      board[x][y] = piece.symbol
-      end
-    end  
-  end
 
-  def print_board
-    print "\e[32m   a  b  c  d  e  f  g  h\n\e[0m"
-    board.each_with_index do |line, index|
-      changed = line.map.each_with_index {|spot, ind| select_background(index, ind, spot) }.join
-      puts "\e[34m#{(board.size) - index}\e[0m #{changed} \e[34m#{(board.size) - index}\e[0m"
-    end
-    puts "\e[32m   a  b  c  d  e  f  g  h\n\e[0m"
-  end
-
-  def select_background(row_index, column_index, spot)
-    if spot.nil?
-      (row_index + column_index).even? ? "\e[46m   \e[0m" : "\e[44m   \e[0m" 
-    else
-      (row_index + column_index).even? ? "\e[46m #{spot} \e[0m" : "\e[44m #{spot} \e[0m"  
-    end   
-  end
 end
 
 class Game
@@ -55,11 +31,18 @@ class Game
   attr_reader :players, :pieces
 
   def initialize
-    @board = Array.new(8) { Array.new(8) }
+    @board = Board.new
     @current_player_id = 0
     @players = [Player.new('white'), Player.new('black')]
-    @pieces = create_pieces('white'), create_pieces('black')
-    place_initial_pieces() 
+    @pieces = create_pieces('white') + create_pieces('black')
+    @board.place_initial_pieces(@pieces) 
+  end
+
+  def player_turn
+    start = move_from()
+    finish = move_to()
+    piece_on_square(start)
+    piece_on_square(finish)
   end
 
   def move_from
@@ -72,6 +55,10 @@ class Game
     puts "Where would you like to move it to?"
     finish = current_player.pick_square
     adjust_player_selection(finish)
+  end
+
+  def piece_on_square(coordinates)
+    p @pieces.select { |piece| piece.current == coordinates }
   end
 
   def current_player
