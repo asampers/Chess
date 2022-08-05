ec47b60cd67664c8b37cf5233efb648d4381780c
nanarama <annasampers@gmail.com>
2022-08-05 16:57:02 -0500

    Methods to generate moves and update current location

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,11 +1,11 @@
 class Knight 
-  attr_accessor :current, :symbol
-  attr_reader :starting, :team
+  attr_accessor :current 
+  attr_reader :starting, :team, :symbol
 
   def initialize(team, num)
     @starting = get_start(team, num)
     @current = @starting
-    @symbol = team == 'white' ? "\u{2658}" : "\u{265E}"
+    @symbol = get_symbol(team)
     @team = team
   end  
   
@@ -15,6 +15,24 @@ class Knight
     else 
       team == 'white' ? [7, 6] : [0, 6]  
     end
+  end
+
+  def get_symbol(team)
+    team == 'white' ? "\u{2658}" : "\u{265E}"
+  end
+
+  def move(destination)
+    @current = destination
+  end
+
+  def possible_moves
+    [[1, 2], [2, 1], [-1, -2], [-2, -1], [1, -2], [2, -1], [-1, 2], [-2, 1]]
+  end
+
+  def possible_coordinates(current=@current)
+    possible_coordinates = possible_moves.map do |move|
+      [current[0] + move[0], current[1] + move[1]]
+    end  
   end
 
   def to_s
