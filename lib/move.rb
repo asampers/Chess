3959d464d5e57f5ee8b647ea940cfe040c44cbb9
nanarama <annasampers@gmail.com>
2022-08-11 15:33:37 -0500

    Add methods for diagonal movement, udpate horizontal and vertical movement. So so ugly, need to clean up

--- lib/move.rb
+++ lib/move.rb
@@ -4,28 +4,76 @@ module Move
     @current = destination
   end
 
-  def keep_legal_first_visit(board, moves)
+  def keep_legal_moves(board=Board.new, moves)
     moves.keep_if {|move| board.legal_move?(move)}
     moves
   end
 
-  def first_visit?(move, turns)
-    turns.each do |turn| 
-      if turn.include?(move)
-        return false
-      end 
-    end 
-    true  
-  end
-
   def possible_moves(start=@current)
     moves = move_options.map do |move|
-     [start[0] + move[0], start[1] + move[1]]
-    end 
-    keep_legal_first_visit(board=Board.new, moves) 
+      [start[0] + move[0], start[1] + move[1]]
+    end
+    keep_legal_moves(moves) 
   end
 
-  def clear_path?(path, board=@board)
+  def vertical_moves(start=@current)
+    moves = vertical_move_options.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def vertical_moves2(start=@current)
+    moves = vertical_move_options2.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def horizontal_moves(start=@current)
+    moves = horizontal_move_options.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def horizontal_moves2(start=@current)
+    moves = horizontal_move_options2.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def diagonal_down_moves(start=@current)
+    moves = diagonal_down_move_options.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def diagonal_down_moves2(start=@current)
+    moves = diagonal_down_move_options2.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end  
+
+  def diagonal_up_moves(start=@current)
+    moves = diagonal_up_move_options.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def diagonal_up_moves2(start=@current)
+    moves = diagonal_up_move_options2.map do |move|
+      [start[0] + move[0], start[1] + move[1]]
+    end 
+    keep_legal_moves(moves)
+  end
+
+  def can_move?(path, moves, finish, board=@board)
+    return true if moves.include?(finish) 
     return true if path.length == 1
     return true if path.all? {|space| board.square_free?(space)}
     
@@ -33,11 +81,12 @@ module Move
   end
 
   def find_path(moves, finish)
+    option = moves.select {|turn| turn.include?(finish)}.pop
     path = []
-    moves.each do |move| 
+    return moves if option.nil?
+    option.each do |move|
+      return path if move == finish
       path << move
-      return path if move == finish    
-    end
+    end 
   end
-
 end
