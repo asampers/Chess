aac33c6bb77c74954141482e8e1a0b876b3e2031
nanarama <annasampers@gmail.com>
2022-08-15 11:10:27 -0500

    Fix possible_moves method

--- lib/queen.rb
+++ lib/queen.rb
@@ -5,13 +5,23 @@ class Queen
   attr_reader :starting, :team, :symbol
 
   def initialize(team)
-    @current = team == 'white' ? [7, 3] : [0, 3]
+    @current = get_start(team)
     @symbol = team == 'white' ? "\u{2655}" : "\u{265B}" 
     @team = team
   end  
 
+  def get_start(team)
+    team == 'white' ? [7, 3] : [0, 3]
+  end 
+
+  def can_move?(path, moves, finish, board)
+    return true if path.all? {|space| board.square_free?(space)}
+
+    false
+  end 
+
   def possible_moves(start=@current)
-    moves = get_move_options.each do |option| 
+    moves = get_move_options.map do |option| 
               option.map do |move|
                 [start[0] + move[0], start[1] + move[1]]
               end 
@@ -20,14 +30,14 @@ class Queen
   end
 
   def get_move_options
-    [[0,-1], [0,-2], [0,-3], [0,-4], [0,-5], [0,-6], [0,-7],
+    [[[0,-1], [0,-2], [0,-3], [0,-4], [0,-5], [0,-6], [0,-7]],
     [[0, 1], [0, 2], [0,3], [0,4], [0,5], [0,6], [0,7]],
     [[-1,0], [-2, 0], [-3,0], [-4,0], [-5,0], [-6,0], [-7,0]],
     [[1,0], [2,0], [3,0], [4,0], [5,0], [6,0], [7,0]],
     [[-1,-1], [-2,-2], [-3,-3], [-4,-4], [-5,-5], [-6,-6], [-7,-7]],
     [[-1,1], [-2,2], [-3,3], [-4,4], [-5,5], [-6,6], [-7,7]],
     [[1,1], [2,2], [3,3], [4,4], [5,5], [6,6], [7,7]],
-    [[1,-1], [2,-2],  [3,-3], [4,-4], [5,-5], [6,-6], [7,-7]]]
+    [[1,-1], [2,-2], [3,-3], [4,-4], [5,-5], [6,-6], [7,-7]]]
   end
  
   def to_s
