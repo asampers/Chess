4fec32d116b1b47b66f9eaa2508eca63c85c1d4a
nanarama <annasampers@gmail.com>
2022-08-18 15:46:45 -0500

    Update can_move? method

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -8,13 +8,17 @@ class Pawn
   def initialize(team, num)
     @starting = get_start(team, num)
     @current = @starting
-    @symbol = team == 'white' ? "\u{2659}" : "\u{265F}"
+    @symbol = get_symbol(team)
     @team = team
   end  
 
   def get_start(team, num)
     team == 'white' ? [6, num] : [1, num]  
   end 
+
+  def get_symbol(team)
+    team == 'white' ? "\u{2659}" : "\u{265F}"
+  end
 
   def possible_moves(start=@current)
     moves = get_move_options.map do |option| 
@@ -27,11 +31,11 @@ class Pawn
 
   def get_move_options
     if @team == 'white' && @starting == @current
-      [[[-1,0], [-2,0]], [[-1,-1], [-1, 1], [-2,-1], [-2,1]]]
+      [[[-1,0], [-2,0]], [[-1,-1], [-1, 1]]]
     elsif @team == 'white'
       [[[-1,0]], [[-1,-1], [-1,1]]]
     elsif @team == 'black' && @starting == @current
-      [[[1,0], [2,0]], [[1,-1], [1,1], [2,-1], [2,1]]]
+      [[[1,0], [2,0]], [[1,-1], [1,1]]]
     else
       [[[1,0]], [[1,-1], [1,1]]] 
     end     
@@ -40,11 +44,9 @@ class Pawn
   def can_move?(path, moves, finish, board)
     if board.square_free?(finish)
       moves.delete_at(1)
-      return moves.flatten!(1).include?(finish) && moves.all? {|space| board.square_free?(space)}
-    elsif board.square_free?(finish) == false
-      return moves.flatten(1).include?(finish)
     end
     
+    return moves.flatten(1).include?(finish) && path.all? {|space| board.square_free?(space)}
   end 
 
   
