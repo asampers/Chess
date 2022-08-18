d56a823ac7496c0c949b027d6d648bfa9fd6a27a
nanarama <annasampers@gmail.com>
2022-08-18 15:48:11 -0500

    Update start and symbol method

--- lib/king.rb
+++ lib/king.rb
@@ -1,20 +1,31 @@
 class King
   include Move
+  include Check
 
   attr_accessor :current
   attr_reader :starting, :team, :symbol
 
   def initialize(team)
-    @starting = team == 'white' ? [7, 4] : [0, 4]
+    @starting = get_start()
     @current = @starting
-    @symbol = team == 'white' ? "\u{2654}" : "\u{265A}"
+    @symbol = get_symbol()
     @team = team
   end  
   
+  def get_start(team)
+    team == 'white' ? [7, 4] : [0, 4]
+  end
+
+  def get_symbol(team)
+    team == 'white' ? "\u{2654}" : "\u{265A}"
+  end
+
   def move_options
     [[-1,0], [1,0], [0,-1], [0, 1], 
     [-1,-1],[-1,1], [1,-1], [1, 1]]
   end
+
+  
 
   def to_s
     "King"
