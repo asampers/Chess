047ce0a9bcd58fb741b31111d96dc9c148e77f68
nanarama <annasampers@gmail.com>
2022-08-18 14:04:06 -0500

    Adjust get_move_options and can_move? to allow for correct diagonal capturing only

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,5 +1,6 @@
 class Pawn
   include Move 
+  include Check
 
   attr_accessor :current
   attr_reader :starting, :team, :symbol
@@ -15,37 +16,38 @@ class Pawn
     team == 'white' ? [6, num] : [1, num]  
   end 
 
-  def move_options
-    get_move_options()
+  def possible_moves(start=@current)
+    moves = get_move_options.map do |option| 
+              option.map do |move|
+                [start[0] + move[0], start[1] + move[1]]
+              end 
+            end
+    moves.map {|move| keep_legal_moves(move)} 
   end
-  
+
   def get_move_options
     if @team == 'white' && @starting == @current
-      [[-1,0], [-2,0]]
+      [[[-1,0], [-2,0]], [[-1,-1], [-1, 1], [-2,-1], [-2,1]]]
     elsif @team == 'white'
-      [[-1,0]]
+      [[[-1,0]], [[-1,-1], [-1,1]]]
     elsif @team == 'black' && @starting == @current
-      [[2,0], [1,0]]
+      [[[1,0], [2,0]], [[1,-1], [1,1], [2,-1], [2,1]]]
     else
-      [[1,0]] 
+      [[[1,0]], [[1,-1], [1,1]]] 
     end     
   end
 
-  def move_diagonally(moves)
-    diagonal = moves.map do |move|
-      [move[0] + 0, move[1] - 1]  
-    end  
-    diagonal2 = moves.map do |move|
-      [move[0] + 0, move[1] + 1]
-    end  
-    moves = moves + diagonal + diagonal2
-    keep_legal_moves(moves)
+  def can_move?(path, moves, finish, board)
+    if board.square_free?(finish)
+      moves.delete_at(1)
+      return moves.flatten!(1).include?(finish) && moves.all? {|space| board.square_free?(space)}
+    elsif board.square_free?(finish) == false
+      return moves.flatten(1).include?(finish)
+    end
+    
   end 
 
-  def can_move?(path, moves, finish, board)
-    return true if pawn_capturing?(moves, finish, board)
-    return moves.include?(finish) && path.all? {|space| board.square_free?(space)}
-  end 
+  
 
   def to_s
     "Pawn"
